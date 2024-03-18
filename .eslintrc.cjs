2aa16b2e58ea0715b77d3c96d48872d36c1ad6f3
Max Milton <max@happyhackers.xyz>
2024-03-19 09:13:25 +1100

    chore: Migrate from `prettier` to `biome` + update and clean up dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,18 +19,24 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:unicorn/recommended',
-    'prettier',
   ],
-  plugins: ['prettier'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': ERROR,
     'import/prefer-default-export': OFF,
-    'prettier/prettier': WARN,
     'unicorn/filename-case': OFF,
     'unicorn/no-abusive-eslint-disable': WARN,
     'unicorn/no-null': OFF,
     'unicorn/prefer-module': OFF,
     'unicorn/prefer-node-protocol': OFF,
     'unicorn/prevent-abbreviations': OFF,
+
+    /* Covered by biome formatter */
+    '@typescript-eslint/indent': OFF,
+    'function-paren-newline': OFF,
+    'implicit-arrow-linebreak': OFF,
+    'max-len': OFF,
+    'object-curly-newline': OFF,
+    'operator-linebreak': OFF,
+    'unicorn/no-nested-ternary': OFF,
   },
 };
