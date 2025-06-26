3e51f61ca4ed0340851c5668f75810c94f06c771
Max Milton <max@happyhackers.xyz>
2025-06-26 20:13:32 +0900

    feat: Move and rename configs + fix issues

--- src/tailwindcss.js
+++ src/tailwindcss.js
@@ -1,18 +1,22 @@
 /** @type {import('stylelint').Config} */
-export const config = {
+export default {
   rules: {
+    'at-rule-no-deprecated': [true, { ignoreAtRules: ['apply'] }],
     'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: [
           /** tailwindcss v4 */
+          // https://tailwindcss.com/docs/functions-and-directives
           'theme',
           'source',
           'utility',
           'variant',
           'custom-variant',
           'plugin',
+          'reference',
           /** tailwindcss v3 */
+          // https://v3.tailwindcss.com/docs/functions-and-directives
           'tailwind',
           'apply',
           'layer',
@@ -20,13 +24,18 @@ export const config = {
           /** tailwindcss v1, v2 */
           // 'variants',
           // 'responsive',
-          // 'screen',
+          'screen',
         ],
       },
     ],
-    'function-no-unknown': [true, { ignoreFunctions: ['theme'] }],
+    'declaration-property-value-no-unknown': [
+      true,
+      { ignoreProperties: { '/.+/': ['/theme(.+)/'] } },
+    ],
+    'function-no-unknown': [
+      true,
+      { ignoreFunctions: ['--alpha', '--spacing', 'theme'] },
+    ],
     'import-notation': null,
   },
 };
-
-export default config;
---
+++ src/xcss.js
@@ -0,0 +1,18 @@
+/** @type {import('stylelint').Config} */
+export default {
+  rules: {},
+  overrides: [
+    {
+      files: ['**/*.xcss'],
+      customSyntax: 'postcss-ekscss',
+      rules: {
+        'declaration-property-value-no-unknown': [
+          true,
+          { ignoreProperties: { '#apply': '/.+/' } },
+        ],
+        'import-notation': null,
+        'property-no-unknown': [true, { ignoreProperties: ['#apply'] }],
+      },
+    },
+  ],
+};
